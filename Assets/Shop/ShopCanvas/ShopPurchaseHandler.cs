849dd1b800df083e5973e7ea1bd4e5e1efb101ff
minhyeon <guncraft11@gmail.com>
2026-05-06 12:06:07 +0900

    Feat: 구매 성공/실패 소리 추가

--- Assets/Shop/ShopCanvas/ShopPurchaseHandler.cs
+++ Assets/Shop/ShopCanvas/ShopPurchaseHandler.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 
+[RequireComponent(typeof(AudioSource))]
 public class ShopPurchaseHandler : MonoBehaviour
 {
     private TextMeshProUGUI moneyText;
@@ -8,9 +9,17 @@ public class ShopPurchaseHandler : MonoBehaviour
     private int startingMoney = 50000;
     private int currentMoney;
 
+    private AudioSource audioSource;
+    private AudioClip purchaseCompleteSound;
+    private AudioClip purchaseFailSound;
+
     private void Awake()
     {
         currentMoney = startingMoney;
+
+        audioSource = GetComponent<AudioSource>();
+        purchaseCompleteSound = Resources.Load<AudioClip>("Shop/ShopPurchaseCompleteSound1");
+        purchaseFailSound = Resources.Load<AudioClip>("Shop/ShopPurchaseFailSound1");
     }
 
     public void SetMoneyText(TextMeshProUGUI ui)
@@ -32,11 +41,13 @@ public class ShopPurchaseHandler : MonoBehaviour
             UpdateMoneyUI();
 
             Debug.Log($"{spore.SporeName} 구매 성공! -{spore.Price}원");
+            audioSource.PlayOneShot(purchaseCompleteSound);
             // 인벤토리에 Spore 추가 로직 구현 예정
         }
         else
         {
             Debug.LogWarning($"금액 부족! 현재 금액: {currentMoney}원 | 필요: {spore.Price}원");
+            audioSource.PlayOneShot(purchaseFailSound);
             // 금액 부족 팝업 메뉴 구현 예정
         }
     }
--- Assets/Shop/ShopManager.cs
+++ Assets/Shop/ShopManager.cs
@@ -13,23 +13,23 @@ public class ShopManager : MonoBehaviour
     private ShopPurchaseHandler shopPurchaseHandler;
     private void Awake()
     {
-        // 상점 창
+        // 상점 창 연결 및 비활성화
         shopCanvasInstance = Instantiate(shopCanvasPrefab);
         shopCanvasInstance.SetActive(false);
 
-        // 상점 아이템 목록 창
+        // 상점 아이템 목록 창 연결
         content = shopCanvasInstance.transform.Find("ShopPanel/Scroll View/Viewport/Content");
 
         // 상점 닫기 버튼 연결
         Button buyButton = shopCanvasInstance.GetComponentInChildren<Button>(true); 
         buyButton.onClick.AddListener(() => CloseShop());
 
-        // 아이템 구매 로직
+        // 아이템 구매 로직 연결
         shopPurchaseHandler = GetComponent<ShopPurchaseHandler>();
         TextMeshProUGUI moneyTextUI = shopCanvasInstance.GetComponentInChildren<TextMeshProUGUI>(true);
         shopPurchaseHandler.SetMoneyText(moneyTextUI);
 
-        // Resources/Spores의 spore asset에 따라 상점 아이템 목록 자동 제작
+        // Resources/Spores에 위치한 spore asset에 따라 상점 아이템 목록 자동 제작
         PopulateShop();
     }
 
@@ -44,12 +44,12 @@ public class ShopManager : MonoBehaviour
         // 상점 아이템 목록 제작
         foreach (Spore spore in allSpores)
         {
-            GameObject shopItemInstance = Instantiate(shopItemPrefab, content);
+            GameObject shopItemInstance = Instantiate(shopItemPrefab, content); // 상점 아이템 목록에 ShopItem 출력
 
-            RectTransform rect = shopItemInstance.GetComponent<RectTransform>(); // 이 코드 없으면 상점 아이템 안 보임
+            RectTransform rect = shopItemInstance.GetComponent<RectTransform>(); // 이 코드 없으면 아이템이 안 보임;;
             rect.localScale = Vector3.one;
 
-            ShopItem shopItem = shopItemInstance.GetComponent<ShopItem>(); // 각 상점 아이템 정보를 spore 에셋과 연동
+            ShopItem shopItem = shopItemInstance.GetComponent<ShopItem>(); // 각 아이템 정보를 spore 에셋과 연동
             shopItem.SetData(spore); 
             
             Button buyButton = shopItemInstance.GetComponentInChildren<Button>(true); // 구매 버튼을 구매 로직과 연결
