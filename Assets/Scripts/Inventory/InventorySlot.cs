fc625049175c5c05879a3a3017e73703a7566376
강준이 <junyi04@gangjun-iui-MacBookPro.local>
2026-05-17 23:47:33 +0900

    Feat: 빈슬롯 드래그앤드롭 구현

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,8 +1,13 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot : MonoBehaviour,
+    IBeginDragHandler,
+    IDragHandler,
+    IEndDragHandler,
+    IDropHandler
 {
     [Header("UI")]
     [SerializeField] private Image _icon;
@@ -13,9 +18,94 @@ public class InventorySlot : MonoBehaviour
 
     // 현재 아이템 개수
     private int _count;
+    private GameObject _dragIcon;
+    private Canvas _canvas;
 
     public Item Item => _item;
     public int Count => _count;
+
+    private void Start()
+    {
+        _canvas = GetComponentInParent<Canvas>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // 빈 슬롯이면 드래그 불가
+        if (_item == null)
+        {
+            return;
+        }
+
+        _dragIcon = new GameObject("DragIcon");
+        _dragIcon.transform.SetParent(_canvas.transform, false);
+
+        Image image = _dragIcon.AddComponent<Image>();
+
+        image.sprite = _icon.sprite;
+        image.raycastTarget = false;
+
+        RectTransform rect =
+            _dragIcon.GetComponent<RectTransform>();
+
+        rect.sizeDelta = new Vector2(80, 80);
+
+        _dragIcon.transform.position = eventData.position;
+    }
+
+    // 드래그 중 이동
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (_dragIcon == null)
+        {
+            return;
+        }
+
+        _dragIcon.transform.position = eventData.position;
+    }
+
+    // 드래그 종료
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (_dragIcon != null)
+        {
+            Destroy(_dragIcon);
+        }
+    }
+
+    // 드롭 처리
+    public void OnDrop(PointerEventData eventData)
+    {
+        InventorySlot draggedSlot =
+            eventData.pointerDrag.GetComponent<InventorySlot>();
+
+        if (draggedSlot == null)
+        {
+            return;
+        }
+
+        // 자기 자신이면 무시
+        if (draggedSlot == this)
+        {
+            return;
+        }
+
+        // 빈 슬롯이면 이동
+        if (_item == null)
+        {
+            SetItem(draggedSlot.Item, draggedSlot.Count);
+
+            draggedSlot.Clear();
+
+            return;
+        }
+
+        // 아이템 슬롯이면 조합 시도
+        InventoryManager.Instance.TryCombine(
+            draggedSlot.Item,
+            _item
+        );
+    }
 
     // 슬롯에 아이템 설정
     public void SetItem(Item item, int count)
