2fc12dea8f0244d6bb2c765bfdcf05b5f9085d50
LLLeWi <kimth3301@gmail.com>
2026-05-21 01:25:44 +0900

    Fix: 화분 이미지 안맞는 버그 수정, 포자 생성 한 번만 되는 버그 수정

--- Assets/Scripts/Student/Drag.cs
+++ Assets/Scripts/Student/Drag.cs
@@ -4,21 +4,29 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using System;
+using Unity.VisualScripting;
+using UnityEngine.Rendering;
+using System.IO;
 
-public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerUpHandler, IPointerDownHandler 
-{
-    public static event Action<bool> OnDragging;
-    
-    //[HideInInspector] public Transform parentAfterDrag;
-    public UnityEngine.UI.Image image;
-    public int type;  //0:화분  1:포자
-    private RectTransform rectTransform;
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler 
+{   
+    [SerializeField] private UnityEngine.UI.Image image;
+    [SerializeField] private Item _dragItemSO;
+    [SerializeField] private GameObject[] Pots; 
+    private RectTransform _rectTransform;
+
+    private void Start()
+    {
+        VisibleGuide(false);
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-
-        rectTransform = GetComponent<RectTransform>();
+        _rectTransform = GetComponent<RectTransform>();
         transform.SetParent(transform.root);    //Grid에서 벗어나도록
         image.raycastTarget = false;    //raycast가 화분 감지x, PotLocation감지o
+
+        VisibleGuide(true);
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -28,17 +36,74 @@ public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        rectTransform.anchoredPosition = new Vector2(-100f, -100f);
+        OrderUI(_dragItemSO);
+
+        Place(eventData.position); //화분이든 포자든 심어드립니다
+        VisibleGuide(false);
         image.raycastTarget = true;
     }
 
-    public void OnPointerUp(PointerEventData eventData)
+    private void OrderUI(Item draggingItemSO) //화분생성, 연필포자 등의 위치조절
     {
-        OnDragging.Invoke(false);
+        switch (draggingItemSO.ItemName)
+        {
+            case "화분":
+                _rectTransform.anchoredPosition = new Vector2(350f, 175f);
+                break;
+            case "연필 포자":
+                _rectTransform.anchoredPosition = new Vector2(350f, 65f);
+                break;
+            case "교과서 포자":
+                _rectTransform.anchoredPosition = new Vector2(350f, -5f);
+                break;
+            case "칠판 포자":
+                _rectTransform.anchoredPosition = new Vector2(350f, -75f);
+                break;
+            case "급식 포자":
+                _rectTransform.anchoredPosition = new Vector2(350f, -145f);
+                break;
+            default:
+                break;
+        }
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+    private void VisibleGuide(bool isVisible) //배치 가능 위치 표시/안표시
     {
-        OnDragging.Invoke(true);
+        for(int i = 0; i < Pots.Length; i++)
+        {
+            Pot pot = Pots[i].GetComponent<Pot>();
+            if (!pot.IsPotPlaced)
+            {
+                if (isVisible)
+                {
+                    pot.ShowGuide();
+                }
+                else
+                {
+                    pot.HideGuide();
+                }
+            }
+        }
+    }
+
+    private void Place(Vector2 screenPos)
+    {
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+        Collider2D hitCollider = Physics2D.OverlapPoint(worldPos);
+        if (hitCollider != null)
+        {
+            GameObject hitObject = hitCollider.gameObject;
+            Pot pot = hitObject.GetComponent<Pot>();
+            if(_dragItemSO.ItemName == "화분")
+                {
+                    pot.ShowPot();
+                    pot.IsPotPlaced = true;
+                }
+            else
+                {
+                    pot.ShowSpore(_dragItemSO);
+                    pot.IsSporePlaced = true;
+                }
+        }
     }
 }
